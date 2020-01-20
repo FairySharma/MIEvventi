de4d5d71e7add9dd9b28fbc0acccfbd91716f0be
Fairy Sharma <fairysharma1998@gmail.com>
2020-01-20 16:07:19 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ServiceComponent,
+    GalleryComponent,
+    OurteamComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
